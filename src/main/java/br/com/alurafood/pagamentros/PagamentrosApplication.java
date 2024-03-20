1e128cf3996364b8ea89d17e9f20dcd3fd65dea3
Thiago Crepequer <thiago.crepequer@hotmail.com>
2024-03-20 10:07:34 -0300

    init

---
+++ src/main/java/br/com/alurafood/pagamentros/PagamentrosApplication.java
@@ -0,0 +1,13 @@
+package br.com.alurafood.pagamentros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PagamentrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PagamentrosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/alurafood/pagamentros/PagamentrosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alurafood.pagamentros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PagamentrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
