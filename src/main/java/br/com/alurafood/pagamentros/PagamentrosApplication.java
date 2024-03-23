070f56c69d9f2c3e6e35aa71cc759fe97975660d
Thiago Crepequer <thiago.crepequer@hotmail.com>
2024-03-23 16:59:30 -0300

    improvement: adicionado conexão com o Eureka

--- src/main/java/br/com/alurafood/pagamentros/PagamentrosApplication.java
+++ src/main/java/br/com/alurafood/pagamentros/PagamentrosApplication.java
@@ -2,8 +2,10 @@ package br.com.alurafood.pagamentros;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class PagamentrosApplication {
 
 	public static void main(String[] args) {
